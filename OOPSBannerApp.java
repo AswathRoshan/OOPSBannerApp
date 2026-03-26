99e05f831ee4b07fb9e8b47dcd64361a63b9c44f
AswathRoshan <roshanaswath@gmail.com>
2026-03-26 09:23:24 +0530

    feature/UC6: Refactored using pattern methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,34 +1,64 @@
 /**
  * OOPSBannerApp
- * UC5: Modular Banner Rendering using Methods
+ * UC6: Refactor Banner Logic into Functions
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        printBanner();
-    }
 
-    // Method to print banner
-    public static void printBanner() {
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
+        // Build final banner
         String[] banner = new String[7];
 
-        banner[0] = buildLine("  *****  ", "  *****  ", " ******  ", " *****   ");
-        banner[1] = buildLine(" *     * ", " *     * ", " *     * ", " *       ");
-        banner[2] = buildLine(" *     * ", " *     * ", " *     * ", " *       ");
-        banner[3] = buildLine(" *     * ", " *     * ", " ******  ", "  *****  ");
-        banner[4] = buildLine(" *     * ", " *     * ", " *       ", "       * ");
-        banner[5] = buildLine(" *     * ", " *     * ", " *       ", "       * ");
-        banner[6] = buildLine("  *****  ", "  *****  ", " *       ", " *****   ");
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ", o[i], o[i], p[i], s[i]);
+        }
 
-        // Loop to print
+        // Print banner
         for (String line : banner) {
             System.out.println(line);
         }
     }
 
-    // Reusable method to build each line
-    public static String buildLine(String a, String b, String c, String d) {
-        return String.join(" ", a, b, c, d);
+    // Method for O pattern
+    public static String[] getOPattern() {
+        return new String[]{
+                "  *****  ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    // Method for P pattern
+    public static String[] getPPattern() {
+        return new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        };
+    }
+
+    // Method for S pattern
+    public static String[] getSPattern() {
+        return new String[]{
+                " *****   ",
+                " *       ",
+                " *       ",
+                "  *****  ",
+                "       * ",
+                "       * ",
+                " *****   "
+        };
     }
 }
